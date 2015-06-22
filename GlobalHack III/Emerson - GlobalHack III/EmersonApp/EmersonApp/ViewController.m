c148107da1c3679c25174a32456d756965fa5451
Boris Polania <boris.polania@gmail.com>
2015-06-22 15:29:31 -0400

    Adds

---
+++ GlobalHack III/Emerson - GlobalHack III/EmersonApp/EmersonApp/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  EmersonApp
+//
+//  Created by Boris  on 12/6/14.
+//  Copyright (c) 2014 LLT. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
